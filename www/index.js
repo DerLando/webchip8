02f2ef98b045b63f531ba64743105332f82b3f22
Lando <lando.schumpich@gmail.com>
2023-07-25 19:24:42 +0200

    Add memory dump of registers

--- www/index.js
+++ www/index.js
@@ -41,6 +41,7 @@ const memoryTableAddresses = [
   document.getElementById("mem_ptr_2"),
   document.getElementById("mem_ptr_3"),
   document.getElementById("mem_ptr_4"),
+  document.getElementById("mem_ptr_5"),
 ];
 const memoryTableValues = [
   document.getElementById("mem_val_-4"),
@@ -52,6 +53,7 @@ const memoryTableValues = [
   document.getElementById("mem_val_2"),
   document.getElementById("mem_val_3"),
   document.getElementById("mem_val_4"),
+  document.getElementById("mem_val_5"),
 ];
 const format_number_as_hex = (number) => {
   return number.toString(16).toUpperCase();
@@ -59,11 +61,45 @@ const format_number_as_hex = (number) => {
 const updateMemory = () => {
   let memoryDump = emulator.dump_memory_u16();
   let pc = emulator.dump_pc() - 8;
-  for (let i = 1; i < memoryDump.length - 1; i++) {
+  for (let i = 1; i < memoryDump.length; i++) {
     memoryTableAddresses[i - 1].innerHTML = format_number_as_hex(pc);  
     memoryTableValues[i - 1].innerHTML = format_number_as_hex(memoryDump[i]);  
     pc += 2;
   }
+}
+
+const registerTableValues = [
+  document.getElementById("reg_val_0"),
+  document.getElementById("reg_val_1"),
+  document.getElementById("reg_val_2"),
+  document.getElementById("reg_val_3"),
+  document.getElementById("reg_val_4"),
+  document.getElementById("reg_val_5"),
+  document.getElementById("reg_val_6"),
+  document.getElementById("reg_val_7"),
+  document.getElementById("reg_val_8"),
+  document.getElementById("reg_val_9"),
+  document.getElementById("reg_val_a"),
+  document.getElementById("reg_val_b"),
+  document.getElementById("reg_val_c"),
+  document.getElementById("reg_val_d"),
+  document.getElementById("reg_val_e"),
+  document.getElementById("reg_val_f"),
+]
+const soundRegisterCell = document.getElementById("reg_val_sound");
+const delayRegisterCell = document.getElementById("reg_val_delay");
+const iRegisterCell = document.getElementById("reg_val_i");
+const pcCell = document.getElementById("reg_val_pc");
+
+const updateRegisters = () => {
+  let registerValues = emulator.dump_registers();
+  for(let i = 0; i < registerValues.length; i++) {
+    registerTableValues[i].innerHTML = registerValues[i];
+  }
+  // soundRegisterCell.innerHTML = emulator.dump_sound();
+  delayRegisterCell.innerHTML = emulator.dump_delay();
+  iRegisterCell.innerHTML = format_number_as_hex(emulator.dump_i());
+  pcCell.innerHTML = format_number_as_hex(emulator.dump_pc());
 }
 
 document.addEventListener('keydown', (event) => {
@@ -79,6 +115,7 @@ let animationId = null;
 
 const renderLoop = () => {
   updateMemory();
+  updateRegisters();
   drawDisplay();
   emulator.tick();
   animationId = requestAnimationFrame(renderLoop);
